f6b8a995e5692e4ddd7caae7efd92ea453929981
gaoletian <gaolet@chanjet.com>
2020-04-30 15:02:15 +0800

    fix: TsconfigPathsPlugin undefine

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
 /**
  * cejt会将此文件中的webpack配置与内置配置自动合并
  * 可以在此文件中根据项目情况扩展webpack更多的配置，如扩展更多loader、plugin等。
